9d5adad9e68573ac26ea5ae063d7d3ae69239d27
Allan Ortiz <aortez@gmail.com>
2021-11-01 21:09:51 -0700

    add a basic vector type

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-This project is just an experiment to learn Go.
-
-The idea is to build a little neural network testbed,
-in which it controls an organism that is looking for food.
+# What is this?
+This project is just an experiment to learn Go and mess around with neural networks.
 
 The app is intended to be published at allan.pizza to replace the aging
 webcomponents-based implementation that is currently there.
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
     "syscall/js"
     "time"
 
+    "test-webassembly/vec2"
+
     "github.com/llgcode/draw2d/draw2dimg"
     "github.com/llgcode/draw2d/draw2dkit"
     "github.com/markfarnan/go-canvas/canvas"
@@ -13,11 +15,9 @@ import (
 
 type Ball struct {
     color color.RGBA
-    radius,
-    x,
-    y,
-    xv,
-    yv float64
+    radius float64
+    center vec2.Vec2
+    v vec2.Vec2
 }
 
 var balls [] Ball
@@ -28,14 +28,7 @@ var myCanvas *canvas.Canvas2d
 var width float64
 var height float64
 
-// This specifies how long a delay between calls to 'render'.     To get Frame Rate,   1s / renderDelay
-var renderDelay time.Duration = 16 * time.Millisecond
-
 func main() {
-    FrameRate := time.Second / renderDelay
-    println("requested FPS:", FrameRate)
-    //canvas, _ = canvas.NewCanvas2d(true)
-
     // Make Canvas 90% of window size.
     myCanvas, _ = canvas.NewCanvas2d(false)
     myCanvas.Create(int(js.Global().Get("innerWidth").Float() * 0.9), int(js.Global().Get("innerHeight").Float() * 0.9))
@@ -45,19 +38,22 @@ func main() {
 
     rand.Seed(time.Now().UnixNano())
 
-    for i := 0; i < 100; i++ {
-        ball := Ball{ radius: 35, xv: 13.7, yv: -13.7, x: 40, y: 40, color: color.RGBA{0xff, 0x00, 0xff, 0xff} }
+    for i := 0; i < 10; i++ {
+        ball := Ball{ radius: 35, v: vec2.Vec2{ X: 13.7, Y: -13.7 }, center: vec2.Vec2{ X: 40, Y: 40 }, color: color.RGBA{ 0xff, 0x00, 0xff, 0xff } }
         balls = append(balls, ball)
         balls = append(balls, Ball{
             radius: rand.Float64() * 50,
-            xv: (rand.Float64() - 0.5) * 30, yv: (rand.Float64() - 0.5) * 30,
-            x: rand.Float64() * width, y: rand.Float64() * height,
+            v: vec2.Vec2 {
+                X: (rand.Float64() - 0.5) * 30,
+                Y: (rand.Float64() - 0.5) * 30},
+            center: vec2.Vec2 {
+                X: rand.Float64() * width,
+                Y: rand.Float64() * height},
             color: color.RGBA{uint8(rand.Float32() * 255), uint8(rand.Float32() * 255), uint8(rand.Float32() * 255), uint8(rand.Float32() * 255)} })
     }
 
     myCanvas.Start(60, Render)
 
-    //go doEvery(renderDelay, Render) // Kick off the Render function as go routine as it never returns
     <-done
 }
 
@@ -71,33 +67,30 @@ func doEvery(d time.Duration, f func(time.Time)) {
 // Called from the 'requestAnimationFrame' function.
 // It may also be called separately from a 'doEvery' function, if the user prefers drawing to be separate from the animationFrame callback.
 func Render(context *draw2dimg.GraphicContext) bool {
-  var backGroundColor color.RGBA = color.RGBA{0x00, 0x00, 0x00, 0xff}
-  context.SetFillColor(backGroundColor)
-  context.Clear()
+    var backGroundColor color.RGBA = color.RGBA{0x00, 0x00, 0x00, 0xff}
+    context.SetFillColor(backGroundColor)
+    context.Clear()
 
-  for index := 0; index < len(balls); index++ {
+    for index := 0; index < len(balls); index++ {
+        ball := &balls[index]
 
-    ball := balls[index]
+        if ball.center.X + ball.v.X > width - ball.radius || ball.center.X + ball.v.X < ball.radius {
+            ball.v.X = -ball.v.X
+        }
+        if ball.center.Y + ball.v.Y > height - ball.radius || ball.center.Y + ball.v.Y < ball.radius {
+            ball.v.Y = -ball.v.Y
+        }
 
-    if ball.x + ball.xv > width - ball.radius || ball.x + ball.xv < ball.radius {
-      ball.xv = -ball.xv
-    }
-    if ball.y + ball.yv > height-ball.radius || ball.y + ball.yv < ball.radius {
-      ball.yv = -ball.yv
-    }
+        ball.center.X += ball.v.X
+        ball.center.Y += ball.v.Y
 
-    ball.x += ball.xv
-    ball.y += ball.yv
+        context.SetFillColor(ball.color)
+        context.SetStrokeColor(ball.color)
 
-    context.SetFillColor(ball.color)
-    context.SetStrokeColor(ball.color)
-
-    context.BeginPath()
-    // context.ArcTo(ball.x, ball.y, ball.size, ball.size, 0, math.Pi*2)
-    draw2dkit.Circle(context, ball.x, ball.y, ball.radius)
-    context.FillStroke()
-
-    balls[index] = ball
+        context.BeginPath()
+        // context.ArcTo(ball.x, ball.y, ball.radius, ball.radius, 0, 6.12)
+        draw2dkit.Circle(context, ball.center.X, ball.center.Y, ball.radius)
+        context.FillStroke()
   }
 
   context.Close()
